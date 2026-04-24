97f13a0cefd966a806fa5232556e105889f51216
lozplyr89 <lozplyr89@gmail.com>
2026-04-24 07:52:29 -0400

    I got nvim to match kitty but treesitter sucks

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,13 @@ local Plug = vim.fn['plug#']
 --Various configs
 vim.wo.number = true --See absolute line cursor is on
 vim.wo.relativenumber = true --See line distance away from cursor
-vim.o.clipboard = "unnamedplus"
+
+vim.o.clipboard = "unnamedplus" --ties "yank" commands to the system keyboard
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"}) --To match kitty theme
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) --To match kitty theme
+
+
 
 --Keybinds
 -- Swap j and k
@@ -16,12 +22,13 @@ vim.keymap.set({'n', 'v', 'o'}, 'k', 'j', {noremap = true})
 vim.call('plug#begin')
 
 --List Plugins here
-	Plug 'm4xshen/autoclose.nvim'
+	Plug 'm4xshen/autoclose.nvim' -- [ = []
+	Plug 'nvim-treesitter/nvim-treesitter'
 	
 vim.call('plug#end')
 
 --Plugin Config
-require("autoclose").setup({
+require("autoclose").setup({ --for autoclose
    keys = {
       ["("] = { escape = false, close = true, pair = "()" },
       ["["] = { escape = false, close = true, pair = "[]" },
@@ -46,5 +53,21 @@ require("autoclose").setup({
    },
 })
 
---LUA ROCKS
-	
+require ("nvim-treesitter.config").setup({
+	 auto_install = true,
+	-- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    additional_vim_regex_highlighting = false,
+  },
+
+  vim.api.nvim_create_autocmd('FileType', {
+	pattern = { '<filetype>' },
+	callback = function() vim.treesitter.start() end,
+	})
+})

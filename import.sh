5b7d2b998b935ce64ccb4b6fe00bf1ee53dbc4c6
lozplyr89 <lozplyr89@gmail.com>
2026-04-27 21:08:31 -0400

    Minor import script adjustment, modified vertical monitor wallpaper

--- import.sh
+++ import.sh
@@ -1,6 +1,6 @@
 echo "Importing from ~/.config/"
 cp -rf ~/.config/hypr/ ~/Main/git/dot-files/
-cp -rf ~/.config/kitty/  ~/Main/git/dot-files/
+cp -f ~/.config/kitty/kitty.conf  ~/Main/git/dot-files/kitty/
 cp -rf ~/.config/nvim/  ~/Main/git/dot-files/
 cp -rf ~/.config/waybar/  ~/Main/git/dot-files/
 cp -rf ~/.config/wofi/  ~/Main/git/dot-files/
